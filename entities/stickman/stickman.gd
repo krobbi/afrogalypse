5dfb69f17cf58f450433f2b5f7f3fc6f258e574f
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-07-07 20:19:10 +0100

    Add more stickman frames

--- entities/stickman/stickman.gd
+++ entities/stickman/stickman.gd
@@ -5,5 +5,6 @@ extends CharacterBody2D
 @export var _acceleration: float = 256.0
 
 func _physics_process(delta: float) -> void:
-	velocity = velocity.move_toward(Input.get_vector("move_left", "move_right", "move_up", "move_down") * _speed, _acceleration * delta)
+	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = velocity.move_toward(input * _speed, _acceleration * delta)
 	move_and_slide()
