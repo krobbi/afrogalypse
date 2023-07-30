5db9920458e95c22e4d53527ba8aa4c94f71430b
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-07-30 15:18:48 +0100

    Handle window close requests with quit requests

--- autoloads/scene_manager/scene_manager.gd
+++ autoloads/scene_manager/scene_manager.gd
@@ -1,8 +1,28 @@
 ## Manages scene transitions.
 extends CanvasLayer
 
+## Whether the scene manager is quitting.
+var _is_quitting: bool = false
+
 ## The [ColorRect] to fade out with.
 @onready var _fade_color: ColorRect = $FadeColor
+
+## Run when the scene manager receives a notification. Make a quit request when
+## a window close request is received.
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		request_quit()
+
+
+## Make a request to quit the game.
+func request_quit() -> void:
+	if _is_quitting:
+		return
+	
+	_is_quitting = true
+	Global.save_data()
+	get_tree().quit()
+
 
 ## Change the current scene from a file path.
 func change_scene(path: String) -> void:
@@ -11,9 +31,3 @@ func change_scene(path: String) -> void:
 	get_tree().change_scene_to_file(path)
 	await create_tween().tween_property(_fade_color, "modulate", Color.TRANSPARENT, 0.4).finished
 	hide()
-
-
-## Make a request to quit the game.
-func request_quit() -> void:
-	Global.save_data()
-	get_tree().quit()
