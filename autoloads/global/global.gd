eb4e9aac33fbf040182b1e7cf952a942a77fd2f6
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-08-04 20:59:57 +0100

    Move game setup code to scene manager

--- autoloads/global/global.gd
+++ autoloads/global/global.gd
@@ -8,19 +8,8 @@ signal game_over_cleared
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 var is_main_card: bool = true
 
-## Run when the global state is ready. Unpause the game and set the locale.
-func _ready() -> void:
-	get_tree().paused = false
-	TranslationServer.set_locale("en")
-
-
-## Reset the gameplay [RandomNumberGenerator] to a known state.
-func reset_rng() -> void:
-	rng.seed = hash("Frog RNG.")
-	rng.state = 0xf706
-
-
 ## Start a new game.
 func new_game() -> void:
-	reset_rng()
+	rng.seed = hash("Frog RNG.")
+	rng.state = 0xf706
 	new_game_started.emit()
--- autoloads/scene_manager/scene_manager.gd
+++ autoloads/scene_manager/scene_manager.gd
@@ -4,6 +4,12 @@ extends CanvasLayer
 ## The [ColorRect] to fade out with.
 @onready var _fade_color: ColorRect = $FadeColor
 
+## Run when the scene manager is ready. Unpause the game and set the locale.
+func _ready() -> void:
+	get_tree().paused = false
+	TranslationServer.set_locale("en")
+
+
 ## Change the current scene from a file path.
 func change_scene(path: String) -> void:
 	show()
