b8562226b3329264b7c788d189ecee0e693bce9f
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-08-08 14:50:45 +0100

    Prepare GUI card system for stack

--- gui/gui_card.gd
+++ gui/gui_card.gd
@@ -2,14 +2,18 @@
 class_name GUICard
 extends PanelContainer
 
-## Emitted when a request is made to change or close the GUI card.
+## Emitted when a request is made to change to a root GUI card.
 signal change_requested(card_name: String)
 
-## Make a request to change the GUI card. Emit [signal change_requested].
+## Emitted when a request is made to clear the stack of GUI cards.
+signal close_requested
+
+## Make a request to change to a root GUI card. Emit [signal change_requested].
 func change_card(card_name: String) -> void:
 	change_requested.emit(card_name)
 
 
-## Make a request to close the GUI card. Emit [signal change_requested].
+## Make a request to clear the stack of GUI cards. Emit
+## [signal close_requested].
 func close_card() -> void:
-	change_requested.emit("")
+	close_requested.emit()
--- gui/gui_card_manager.gd
+++ gui/gui_card_manager.gd
@@ -5,48 +5,70 @@ extends Control
 ## Whether the current [GUICard] was accessed from the [MainGUICard].
 static var is_from_main_card: bool = false
 
+## The stack of current [GUICard] names.
+var _card_stack: Array[String] = []
+
 ## The current [GUICard] to display. [code]null[/code] if no [GUICard] is being
 ## displayed.
 var _card: GUICard = null
 
-## Whether the current [GUICard] is changing.
-var _is_changing: bool = false
+## Whether the current [GUICard] is updating.
+var _is_updating: bool = false
 
 ## The [AudioStreamPlayer] to play when a [GUICard] is changed or closed.
 @onready var _navigate_player: AudioStreamPlayer = $NavigatePlayer
 
-## Change the current [GUICard] from its name. Close the current [GUICard] if
-## [param card_name] is empty.
+## Change to a root [GUICard] from its name.
 func change_card(card_name: String) -> void:
-	if _is_changing:
-		return
-	
-	_is_changing = true
-	
-	if card_name == "main":
-		is_from_main_card = true
-	elif card_name == "options":
-		is_from_main_card = false
+	if not _is_updating:
+		_card_stack.resize(1)
+		_card_stack[0] = card_name
+		_update_card()
+
+
+## Clear the stack of [GUICard]s.
+func close_card() -> void:
+	if not _is_updating:
+		_card_stack.clear()
+		_update_card()
+
+
+## Update the current [GUICard] from the card stack.
+func _update_card() -> void:
+	_is_updating = true
 	
 	if _card:
+		if _card.change_requested.is_connected(change_card):
+			_card.change_requested.disconnect(change_card)
+		
+		if _card.close_requested.is_connected(close_card):
+			_card.close_requested.disconnect(close_card)
+		
 		_navigate_player.play()
 		await create_tween().tween_property(_card, "modulate", Color.TRANSPARENT, 0.1).finished
 		_card.queue_free()
 	
-	if card_name.is_empty():
+	if _card_stack.is_empty():
 		_card = null
-		_is_changing = false
-		return
+	else:
+		var card_name: String = _card_stack[-1]
+		
+		# Should probably find a better solution for this.
+		if card_name == "main":
+			is_from_main_card = true
+		elif card_name == "option":
+			is_from_main_card = false
+		
+		_card = load("res://gui/cards/%s_gui_card.tscn" % card_name).instantiate()
+		_card.modulate = Color.TRANSPARENT
+		_card.change_requested.connect(change_card)
+		_card.close_requested.connect(close_card)
+		add_child(_card)
+		await create_tween().tween_property(_card, "modulate", Color.WHITE, 0.1).finished
+		
+		var focus: Node = get_tree().get_first_node_in_group("focus")
+		
+		if focus is Control:
+			focus.grab_focus()
 	
-	_card = load("res://gui/cards/%s_gui_card.tscn" % card_name).instantiate()
-	_card.modulate = Color.TRANSPARENT
-	_card.change_requested.connect(change_card, CONNECT_ONE_SHOT)
-	add_child(_card)
-	await create_tween().tween_property(_card, "modulate", Color.WHITE, 0.1).finished
-	
-	var focus: Node = get_tree().get_first_node_in_group("focus")
-	
-	if focus is Control:
-		focus.grab_focus()
-	
-	_is_changing = false
+	_is_updating = false
